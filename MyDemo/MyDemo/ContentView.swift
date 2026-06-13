41be31d2c6844cb87d970bb0d45f6f71692d8449
Shiv Prakash Gupta <shiva20june@gmail.com>
2026-06-13 18:12:34 +0530

    Initial Commit

---
+++ MyDemo/MyDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyDemo
+//
+//  Created by Shiv Prakash Gupta on 13/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyDemo/MyDemo/MyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDemoApp.swift
+//  MyDemo
+//
+//  Created by Shiv Prakash Gupta on 13/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
