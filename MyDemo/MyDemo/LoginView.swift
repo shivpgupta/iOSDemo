debc7951d875234196e99c716a0e6f897231ae5d
shivpgupta <shiva20june@gmail.com>
2026-06-15 12:30:39 +0530

    add loginView

---
+++ MyDemo/MyDemo/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  MyDemo
+//
+//  Created by Shiv Prakash Gupta on 15/06/26.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
