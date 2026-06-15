f8920e667278573116f07b94495ea8a0c91380e5
shivpgupta <shiva20june@gmail.com>
2026-06-15 15:29:13 +0530

    add signin screen

---
+++ MyDemo/MyDemo/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  MyDemo
+//
+//  Created by Shiv Prakash Gupta on 15/06/26.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
